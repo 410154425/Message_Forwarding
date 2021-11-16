3faa2f0765b0b355442990c763628b68d1148aa0
top大佬 <80568282+410154425@users.noreply.github.com>
2021-11-16 14:16:09 +0800

    Add files via upload

--- testing.sh
+++ testing.sh
@@ -4,9 +4,9 @@
 #
 MODDIR=${0%/*}
 if type curl > /dev/null 2>&1; then
-	MF_notification="$(dumpsys notification --noredact | sed -n 's/\\\\n//g;s/\\n//g;p')"
+	MF_notification="$(dumpsys notification --noredact | sed -n 's/\\//g;p')"
 	if [ "$MF_notification" = "" ]; then
-		MF_notification="$(dumpsys notification | sed -n 's/\\\\n//g;s/\\n//g;p')"
+		MF_notification="$(dumpsys notification | sed -n 's/\\//g;p')"
 		if [ "$MF_notification" = "" ]; then
 			echo "无法获取消息通知列表"
 			exit 0
@@ -16,7 +16,6 @@ if type curl > /dev/null 2>&1; then
 	MF_Message="$(echo -e "$MF_NotificationRecord" | sed -n 's/mAdjustments=\[.*//g;s/stats=SingleNotificationStats{.*//g;p')"
 	echo "serialno.$(getprop ro.serialno | sed -n 's/ //g;$p'),release.$(getprop ro.build.version.release | sed -n 's/ //g;$p'),sdk.$(getprop ro.build.version.sdk | sed -n 's/ //g;$p'),brand.$(getprop ro.product.brand | sed -n 's/ //g;$p'),model.$(getprop ro.product.model | sed -n 's/ //g;$p')"
 	echo "$MF_Message"
-
 else
 	echo "系统缺少curl命令模块：无法转发消息，请安装curl模块后再使用"
 fi
