cf74eb10717de27095cc5c43703d5487072bdacb
top大佬 <80568282+410154425@users.noreply.github.com>
2021-12-27 21:57:36 +0800

    Add files via upload

--- service.sh
+++ service.sh
@@ -1,18 +1,18 @@
 until [ $(getprop sys.boot_completed) -eq 1 ] ; do
   sleep 5
 done
-sleep 10
+sleep 1
 MODDIR=${0%/*}
 chmod 0755 "$MODDIR/up"
 chmod 0755 "$MODDIR/mf.sh"
 chmod 0755 "$MODDIR/upmf.sh"
 chmod 0755 "$MODDIR/testing.sh"
 chmod 0644 "$MODDIR/config.conf"
-sleep 3
+sleep 1
 up=1
 while :;
 do
-if [ "$up" = "60" -o "$up" = "21600" ]; then
+if [ "$up" = "20" -o "$up" = "7200" ]; then
 	"$MODDIR/up" > /dev/null 2>&1 &
 	up=21
 fi
